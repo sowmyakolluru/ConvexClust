1df7f216d27756d2ffd4d1dfde1c65f7325f746a
Sowmya Kolluru <sowmyakolluru@tamu.edu>
2021-12-01 00:43:13 -0600

    compute weight matrix calculations required for the algorithm

--- src/ComputeWeights.cpp
+++ src/ComputeWeights.cpp
@@ -7,17 +7,16 @@
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
+//Function to compute weights using exp(−mu * ||ai − aj||_2^2)
 // X - n x p matrix
-// K - number of clusters
-// M - K x p cluster centers (always given)
-// numIter - maximal number of iterations
+// mu - given positive constant
 // [[Rcpp::export]]
 arma::mat ComputeWeights_c(const arma::mat& X, double mu) {
-  // All input is assumed to be correct
-
   // Initialize some parameters
   int n = X.n_rows;
+  //calculate number of pairs possible to calculate weights matrix w_ij
   int num_pairs = n*(n-1)/2;
+  //initialize weight matrix of num_pairs x 1 dimensions
   arma::mat weights(num_pairs,1, arma::fill::zeros);
   int k = 1;
   for(int i = 0; i < (n-1); i++)
@@ -25,10 +24,9 @@ arma::mat ComputeWeights_c(const arma::mat& X, double mu) {
     for(int j = i+1; j < n; j++)
     {
       weights.row(k-1) = exp(-mu*pow(arma::norm(X.row(i) - X.row(j), "fro"),2));
-      Rcpp::Rcout << -0.5*pow(arma::norm(X.row(i) - X.row(j), "fro"),2) << std::endl;
       k = k + 1;
     }
   }
+  //return weight matrix
   return(weights);
-
 }
