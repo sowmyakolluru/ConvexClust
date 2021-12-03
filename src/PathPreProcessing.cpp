cb1600e457a6b5e572fe4ebfd76980798e8f5273
Sowmya Kolluru <sowmyakolluru@tamu.edu>
2021-12-03 15:57:56 -0600

    create Prepath_1c and Prepath_2c functions that are to be called in other functions

--- src/PathPreProcessing.cpp
+++ src/PathPreProcessing.cpp
@@ -7,13 +7,13 @@
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
-//Function to find indexes
-// i
-// j
-// p
+//Function that performs calculations required for cluster path processing
+// i integer input
+// j integer input
+// p integer input
 
 // [[Rcpp::export]]
-int Index1_c(int i, int j, int p) {
+int Prepath_1c(int i, int j, int p) {
 
   //return the index value
   return(p*(i-1) - i*(i-1)/2 + j - i);
@@ -21,15 +21,19 @@ int Index1_c(int i, int j, int p) {
 
 // [[Rcpp::export]]
 
-//Function to find indexes
-// k
-// n
+//Function that performs calculations required for cluster path processing
+// k is the vector of indexes of elements satisfying the given condition
+// n is the integer passed in the function argument
 
-arma::mat Index2_c(int k, int p) {
-  // Initialize some parameters
-  arma::mat index_matrix(1,2);
-  index_matrix(0,0) = ceil(0.5 * (2 * p-1 - sqrt(pow(2*p-1,2) - 8*k)));
-  index_matrix(0,1) = k - p*(index_matrix(0,0) - 1) + index_matrix(0,0)*(index_matrix(0,0) - 1)/2 + index_matrix(0,0);
+arma::Mat<int> Prepath_2c(arma::uvec k, int n) {
+  // Initialize required parameters
+  arma::Mat<int> index_matrix(k.n_elem,2);
+  for(int i = 0; i < k.n_elem; i++)
+  {
+    index_matrix(i,0) = ceil(0.5 *( 2*n -1 - (sqrt(pow(2*n-1,2) - 8*k(i)))));
+    index_matrix(i,1) = k(i) - n*(index_matrix(i,0) - 1) + index_matrix(i,0)*(index_matrix(i,0) - 1)/2 + index_matrix(i,0);
+  }
   //return index matrix
   return(index_matrix);
 }
+
