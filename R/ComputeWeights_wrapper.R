4ec94990a835c8352eec79fa1d50b4cd5ceb4a84
Sowmya Kolluru <sowmyakolluru@tamu.edu>
2021-12-11 18:02:21 -0600

    add a detailed function documentation to the compute kernel weights R wrapper

--- R/ComputeWeights_wrapper.R
+++ R/ComputeWeights_wrapper.R
@@ -1,12 +1,21 @@
-#' Title
+#' Compute Kernel Weights
 #'
-#' @param X
-#' @param mu
+#' This function computes weights w_ij using exp(−mu * ||ai − aj||_2^2). (i,j) are the pairs possible for given data matrix.
+#' The weight matrix has dimensions of one column with rows of Number of pairs.
+#'
+#' @param X A data matrix of dimensions p x n
+#' @param mu The non-negative parameter that controls the scaling of kernel weights
 #'
 #' @return
+#' Returns a matrix of dimensions number of pairs by 1
 #' @export
 #'
 #' @examples
+#' data(mammals)
+#' X <- as.matrix(mammals[,-1])
+#' X <- t(scale(X,center=TRUE,scale=FALSE))
+#' ComputeWeights(X, mu =1)
+#'
 ComputeWeights <- function(X, mu){
 
   # Call C++ ComputeWeights_c function to calculate the weights required for the algorithm
